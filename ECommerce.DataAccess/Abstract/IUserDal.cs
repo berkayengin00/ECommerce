f6e317928cf841dfc148adf7c4e48f5d5321e6e9
berkayengin00 <126810524+berkayengin00@users.noreply.github.com>
2023-07-23 17:07:34 +0300

    DAL katmanına EfUserDal  ve IUserDal eklendi

---
+++ ECommerce.DataAccess/Abstract/IUserDal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ECommerce.Core.DataAccess.EntityFramework.BaseType;
+using ECommerce.Entity.Concrete;
+
+namespace ECommerce.DataAccess.Abstract
+{
+	public interface IUserDal:IAddedRepository<User>,IAddedRepositoryAsync<User>
+	{
+	}
+}
---
+++ ECommerce.DataAccess/Concrete/EntityFramework/EfUserDal.cs
@@ -0,0 +1,10 @@
+﻿using ECommerce.Core.DataAccess.EntityFramework.Concrete;
+using ECommerce.DataAccess.Abstract;
+using ECommerce.Entity.Concrete;
+
+namespace ECommerce.DataAccess.Concrete.EntityFramework
+{
+	public class EfUserDal:EfRepositoryBase<User>,IUserDal
+	{
+	}
+}
