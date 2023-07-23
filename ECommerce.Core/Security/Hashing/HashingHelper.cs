4788ce249018216a814ee15dd78bd02607b8b768
berkayengin00 <126810524+berkayengin00@users.noreply.github.com>
2023-07-23 16:46:58 +0300

    Hashing Sınıfı eklendi (#1)

---
+++ ECommerce.Core/Security/Hashing/HashingHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Core.Security.Hashing
+{
+    public class HashingHelper
+    {
+        public static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+	        using var hmac = new System.Security.Cryptography.HMACSHA512();
+	        passwordSalt = hmac.Key;
+	        passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+        }
+
+        public static bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+	        using var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt);
+	        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+	        return !computedHash.Where((t, i) => t != passwordHash[i]).Any();
+        }
+    }
+}
