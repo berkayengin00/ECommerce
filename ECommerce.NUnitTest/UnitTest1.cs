f50f35c488e21965e23562fc5d97d3657bdd8148
berkayengin00 <126810524+berkayengin00@users.noreply.github.com>
2023-07-23 08:58:34 +0300

    PasswordHash ve PasswordSalt test edildi.

--- ECommerce.NUnitTest/UnitTest1.cs
+++ ECommerce.NUnitTest/UnitTest1.cs
@@ -1,4 +1,5 @@
 using ECommerce.Core.Result.Concrete;
+using ECommerce.Core.Security.Hashing;
 using ECommerce.DataAccess.Abstract;
 using ECommerce.Entity.Concrete;
 
@@ -13,9 +14,16 @@ namespace ECommerce.NUnitTest
 		}
 
 		[Test]
-		public void Test1()
+		public void PasswordHash_Test()
 		{
-		
+			var password = "berk";
+			HashingHelper.CreatePasswordHash(password, out var passwordHash, out var passwordSalt);
+
+			var result = HashingHelper.VerifyPasswordHash(password, passwordHash, passwordSalt);
+			Assert.AreEqual(true, result);
+
+			var resultFalse = HashingHelper.VerifyPasswordHash("ber", passwordHash, passwordSalt);
+			Assert.AreEqual(false, resultFalse);
 		}
 	}
 }
