fa733e003576da1ac40a240d5b811581533666b5
berkayengin00 <126810524+berkayengin00@users.noreply.github.com>
2023-08-16 21:33:47 +0300

    Serilog eklendi

---
+++ ECommerce.UI/Filter/CustomExceptionFilter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Serilog;
+
+namespace ECommerce.UI.Filter
+{
+	public class CustomExceptionFilter : IExceptionFilter
+	{
+		public void OnException(ExceptionContext context)
+		{
+			Log.Error(context.Exception, "An exception occurred in {Action} action of {Controller}",
+				context.ActionDescriptor.RouteValues["action"],
+				context.ActionDescriptor.RouteValues["controller"]);
+
+			
+			context.Result = new ObjectResult("An error occurred.")
+			{
+				StatusCode = 500
+			};
+
+			context.ExceptionHandled = true;
+		}
+	}
+}
--- ECommerce.UI/Program.cs
+++ ECommerce.UI/Program.cs
@@ -1,28 +1,56 @@
+using Autofac.Core;
+using ECommerce.Core.Extensions.Service;
 using ECommerce.DataAccess.Abstract;
 using ECommerce.DataAccess.Concrete.EntityFramework;
 using ECommerce.DataAccess.Concrete.EntityFramework.Context;
+using ECommerce.UI.Filter;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews(options =>
+{
+	options.Filters.Add<CustomExceptionFilter>();
+});
 
+#region Serilog
 
+Log.Logger = new LoggerConfiguration()
+	.MinimumLevel.Error()
+	.WriteTo.File("C:\\logs\\log-.txt", rollingInterval: RollingInterval.Day)
+	.CreateLogger();
+
+#endregion
+
+#region DB Configuration
 
 var connectionString = builder.Configuration.GetConnectionString("ECommerce");
-builder.Services.AddDbContext<ECommerceContext>(config=> config.UseSqlServer(connectionString));
+builder.Services.AddDbContext<ECommerceContext>(config => config.UseSqlServer(connectionString));
 
-builder.Services.AddScoped<ECommerceContext>();
+#endregion
+
+#region DAL Configuration
+
 builder.Services.AddScoped<IUserDal, EfUserDal>();
 
+
+#endregion
+
+builder.Logging.AddSerilog();
+
 var app = builder.Build();
+
+//logging middleware
+app.UseLogging();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
 	app.UseExceptionHandler("/Home/Error");
 }
+
 app.UseStaticFiles();
 
 app.UseRouting();
