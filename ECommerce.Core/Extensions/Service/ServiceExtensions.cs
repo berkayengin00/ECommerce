056c0ce28c760e7fee7831714bc13cbd783e41dd
berkayengin00 <126810524+berkayengin00@users.noreply.github.com>
2023-08-16 21:34:26 +0300

    Serilog eklendi (#3)

---
+++ ECommerce.Core/Extensions/Service/ServiceExtensions.cs
@@ -0,0 +1,13 @@
+﻿using ECommerce.Core.Log;
+using Microsoft.AspNetCore.Builder;
+
+namespace ECommerce.Core.Extensions.Service
+{
+	public static class LoggingMiddlewareExtension
+	{
+		public static IApplicationBuilder UseLogging(this IApplicationBuilder builder)
+		{
+			return builder.UseMiddleware<LoggingMiddleware>();
+		}
+	}
+}
