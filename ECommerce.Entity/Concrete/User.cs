43e04b04bf9e349a86574812ebc96f642f8adf03
berkayengin00 <126810524+berkayengin00@users.noreply.github.com>
2023-08-17 15:49:10 +0300

    giriş ekranı eklendi düzenleme yapılacak.

--- ECommerce.Entity/Concrete/User.cs
+++ ECommerce.Entity/Concrete/User.cs
@@ -8,23 +8,4 @@ namespace ECommerce.Entity.Concrete
 		public string PasswordSalt { get; set; }
 		public string Email { get; set; }
 	}
-
-	public class RetailCustomer:User
-	{
-		public string FirstName { get; set; }
-		public string Lastname { get; set; }
-		public DateTime Age { get; set; }
-	}
-
-	public class SellerCustomer:User
-	{
-		public string TaxNo { get; set; }
-	}
-
-	public class Employee : User
-	{
-		public string UserName { get; set; }
-		public string FirstName { get; set; }
-		public string LastName { get; set; }
-	}
 }
